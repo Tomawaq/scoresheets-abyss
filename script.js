2e8820ae77052b81e235054c225f27b45df25cfc
Tomawaq <tom@twq.fr>
2022-05-01 18:16:29 +0200

    enable calculator

--- script.js
+++ script.js
@@ -24,13 +24,42 @@ class ScoreSheet {
     }
 }
 
+class Calculator {
+    constructor(element) {
+        this.result = element.querySelector('[data-result]')
+        element.querySelectorAll('[data-number]').forEach(n => n.addEventListener('click', () => this.result.value += n.innerHTML))
+        element.querySelectorAll('[data-operator]').forEach(o => o.addEventListener('click', () => this.result.value += o.innerHTML))
+        element.querySelectorAll('[data-parenthesis]').forEach(e => e.addEventListener('click', () => this.parenthesis()))
+        element.querySelectorAll('[data-equal]').forEach(e => e.addEventListener('click', () => this.compute()))
+        element.querySelectorAll('[data-back]').forEach(e => e.addEventListener('click', () => this.result.value = this.result.value.substr(0,this.result.value.length - 1)))
+        element.querySelectorAll('[data-clear]').forEach(e => e.addEventListener('click', () => this.result.value = ''))
+    }
+
+    parenthesis() {
+        this.result.value += this.parenthesisOpened ? ')' : '('
+        this.parenthesisOpened = !this.parenthesisOpened
+    }
+
+    compute() {
+        let result
+        try {
+            result = eval(this.result.value.replace('x', '*'))
+        } catch (error) {
+        }
+        this.result.value = result ?? ''
+    }
+}
+
 class Modal {
     constructor(id) {
         this.id = id
         this.element = document.querySelector(`dialog#${id}`)
+        if (id == 'calculator') this.calculator = new Calculator(this.element)
         this.element.addEventListener('close', () => {
             if (id == 'settings') return
-            this.target.querySelector('span').innerHTML = this.element.querySelector('[data-result]').value
+            if (id == 'calculator') this.calculator.compute()
+            if (this.element.querySelector('[data-result]').value)
+                this.target.querySelector('span').innerHTML = this.element.querySelector('[data-result]').value
         })
     }
     open(target) {
